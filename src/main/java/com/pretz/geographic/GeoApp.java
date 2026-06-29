59e312a14fd65c200c6413b7425c060bda761618
Przemyslaw Etz <przemyslaw.etz@gmail.com>
2026-06-29 14:57:36 +0200

    [GEOG-1] infra: add initial commit

---
+++ src/main/java/com/pretz/geographic/GeoApp.java
@@ -0,0 +1,13 @@
+package com.pretz.geographic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoApp {
+
+    static void main(String[] args) {
+        SpringApplication.run(GeoApp.class, args);
+    }
+
+}
---
+++ src/test/java/com/pretz/geographic/GeoAppTests.java
@@ -0,0 +1,13 @@
+package com.pretz.geographic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
