80dc8d07aa1c355c0d4d98bd9d7d3787a0c5249b
Przemyslaw Etz <przemyslaw.etz@gmail.com>
2026-06-30 14:32:17 +0200

    [GEOG-2] feat: Add filtering out other dates

--- src/main/java/com/pretz/geographic/application/domain/service/DailyResultCalculator.java
+++ src/main/java/com/pretz/geographic/application/domain/service/DailyResultCalculator.java
@@ -3,9 +3,10 @@ package com.pretz.geographic.application.domain.service;
 import com.pretz.geographic.application.domain.model.DailyEntry;
 import com.pretz.geographic.application.domain.model.DailyResult;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface DailyResultCalculator {
 
-    List<DailyResult> calculateResults(List<DailyEntry> entries);
+    List<DailyResult> calculateResults(List<DailyEntry> entries, LocalDate requestedDate);
 }
