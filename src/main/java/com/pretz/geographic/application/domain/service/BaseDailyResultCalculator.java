26f402f50e6afae6666e9bf2aaf038a21329dbd5
Przemyslaw Etz <przemyslaw.etz@gmail.com>
2026-06-30 14:32:17 +0200

    [GEOG-2] feat: Add filtering out other dates

--- src/main/java/com/pretz/geographic/application/domain/service/BaseDailyResultCalculator.java
+++ src/main/java/com/pretz/geographic/application/domain/service/BaseDailyResultCalculator.java
@@ -4,6 +4,7 @@ import com.pretz.geographic.application.domain.model.DailyEntry;
 import com.pretz.geographic.application.domain.model.DailyResult;
 import com.pretz.geographic.application.domain.model.Game;
 
+import java.time.LocalDate;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -11,14 +12,15 @@ import java.util.stream.Collectors;
 public class BaseDailyResultCalculator implements DailyResultCalculator {
 
     @Override
-    public List<DailyResult> calculateResults(List<DailyEntry> entries) {
+    public List<DailyResult> calculateResults(List<DailyEntry> entries, LocalDate requestedDate) {
 
         return entries.stream()
+                .filter(ent -> requestedDate.equals(ent.date()))
                 .collect(Collectors.groupingBy(DailyEntry::game))
                 .values().stream()
-                .sorted(Comparator.comparing(it -> it.getFirst().game(), Comparator.comparing(Game::name)))
-                .map(it -> new DailyResult(it.getFirst().game(),
-                        it.stream().sorted(Comparator.comparingInt(DailyEntry::points).reversed()).toList()))
+                .sorted(Comparator.comparing(ent -> ent.getFirst().game(), Comparator.comparing(Game::name)))
+                .map(ent -> new DailyResult(ent.getFirst().game(),
+                        ent.stream().sorted(Comparator.comparingInt(DailyEntry::points).reversed()).toList()))
                 .toList();
     }
 }
--- src/test/java/com/pretz/geographic/application/domain/service/BaseDailyResultCalculatorTest.java
+++ src/test/java/com/pretz/geographic/application/domain/service/BaseDailyResultCalculatorTest.java
@@ -17,18 +17,20 @@ class BaseDailyResultCalculatorTest {
 
         Game game1 = new Game("Game1", ScoringSystem.STANDARD);
         Game game2 = new Game("Game2", ScoringSystem.STANDARD);
-        LocalDate date1 = LocalDate.now();
+        LocalDate date1 = LocalDate.of(2026, 6, 30);
 
         var dailyResultCalculator = new BaseDailyResultCalculator();
 
+        LocalDate requestedDate = LocalDate.of(2026, 6, 30);
+
         var result = dailyResultCalculator.calculateResults(List.of(
-                new DailyEntry(game1, date1, "Player1", 900),
-                new DailyEntry(game1, date1, "Player2", 820),
-                new DailyEntry(game1, date1, "Player3", 960),
-                new DailyEntry(game2, date1, "Player1", 430),
-                new DailyEntry(game2, date1, "Player2", 525),
-                new DailyEntry(game2, date1, "Player3", 990))
-        );
+                        new DailyEntry(game1, date1, "Player1", 900),
+                        new DailyEntry(game1, date1, "Player2", 820),
+                        new DailyEntry(game1, date1, "Player3", 960),
+                        new DailyEntry(game2, date1, "Player1", 430),
+                        new DailyEntry(game2, date1, "Player2", 525),
+                        new DailyEntry(game2, date1, "Player3", 990)),
+                requestedDate);
 
         Assertions.assertThat(result).isEqualTo(List.of(
                 new DailyResult(game1, List.of(
@@ -42,16 +44,42 @@ class BaseDailyResultCalculatorTest {
         ));
     }
 
-    //TODO 2.
     @Test
     public void shouldNotTakeIntoAccountEntriesFromDifferentDays() {
 
+        Game game1 = new Game("Game1", ScoringSystem.STANDARD);
+        Game game2 = new Game("Game2", ScoringSystem.STANDARD);
+        LocalDate date1 = LocalDate.of(2026, 6, 30);
+        LocalDate date2 = date1.minusDays(1);
+
+        var dailyResultCalculator = new BaseDailyResultCalculator();
+
+        LocalDate requestedDate = LocalDate.of(2026, 6, 30);
+
+        var result = dailyResultCalculator.calculateResults(List.of(
+                        new DailyEntry(game1, date1, "Player1", 900),
+                        new DailyEntry(game1, date1, "Player2", 820),
+                        new DailyEntry(game1, date1, "Player3", 960),
+                        new DailyEntry(game2, date1, "Player1", 430),
+                        new DailyEntry(game2, date1, "Player2", 525),
+                        new DailyEntry(game2, date1, "Player3", 990),
+                        new DailyEntry(game1, date2, "Player1", 920),
+                        new DailyEntry(game1, date2, "Player2", 910),
+                        new DailyEntry(game1, date2, "Player3", 940),
+                        new DailyEntry(game2, date2, "Player1", 770),
+                        new DailyEntry(game2, date2, "Player2", 725),
+                        new DailyEntry(game2, date2, "Player3", 765)),
+                requestedDate);
+
+        Assertions.assertThat(result).isEqualTo(List.of(
+                new DailyResult(game1, List.of(
+                        new DailyEntry(game1, date1, "Player3", 960),
+                        new DailyEntry(game1, date1, "Player1", 900),
+                        new DailyEntry(game1, date1, "Player2", 820))),
+                new DailyResult(game2, List.of(
+                        new DailyEntry(game2, date1, "Player3", 990),
+                        new DailyEntry(game2, date1, "Player2", 525),
+                        new DailyEntry(game2, date1, "Player1", 430)))
+        ));
     }
-
-    //TODO 3.
-    @Test
-    public void shouldCallEntriesProviderPort() {
-
-    }
-
 }
