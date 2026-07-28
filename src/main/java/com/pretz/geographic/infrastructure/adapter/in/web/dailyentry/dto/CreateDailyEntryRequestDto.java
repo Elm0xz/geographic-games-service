ceed6935b24bc9544883ecc9605b3419ed5297a4
Przemyslaw Etz <przemyslaw.etz@gmail.com>
2026-07-28 15:27:39 +0200

    [GEOG-8] feat: Add api-level dto bean validation

--- src/main/java/com/pretz/geographic/infrastructure/adapter/in/web/dailyentry/dto/CreateDailyEntryRequestDto.java
+++ src/main/java/com/pretz/geographic/infrastructure/adapter/in/web/dailyentry/dto/CreateDailyEntryRequestDto.java
@@ -1,10 +1,18 @@
 package com.pretz.geographic.infrastructure.adapter.in.web.dailyentry.dto;
 
 import com.pretz.geographic.application.port.in.AddDailyEntryCommand;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.PositiveOrZero;
 
 import java.time.LocalDate;
 
-public record CreateDailyEntryRequestDto(GameRefDto game, PlayerRefDto player, LocalDate date, int points) {
+public record CreateDailyEntryRequestDto(
+        @NotNull @Valid GameRefDto game,
+        @NotNull @Valid PlayerRefDto player,
+        @NotNull @PastOrPresent LocalDate date,
+        @PositiveOrZero int points) {
 
     public AddDailyEntryCommand toCommand() {
         return new AddDailyEntryCommand(
