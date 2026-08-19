ddca77313f438d253bc0790d9bcd86662d97b485
Przemyslaw Etz <przemyslaw.etz@gmail.com>
2026-08-19 16:05:49 +0200

    [GEOG-9] refactor: Move sorting daily entries logic to calculator & add validation to domain object

--- src/main/java/com/pretz/geographic/application/domain/model/DailyRanking.java
+++ src/main/java/com/pretz/geographic/application/domain/model/DailyRanking.java
@@ -2,26 +2,33 @@ package com.pretz.geographic.application.domain.model;
 
 import java.time.LocalDate;
 import java.time.temporal.WeekFields;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 
 public record DailyRanking(Game game, LocalDate date, List<DailyEntry> entries) {
 
-    public static final Comparator<DailyEntry> BY_POINTS_DESC = Comparator.comparingInt(DailyEntry::points).reversed();
-
     public DailyRanking {
         Objects.requireNonNull(game, "DailyRanking game must not be null");
         Objects.requireNonNull(date, "DailyRanking date must not be null");
         Objects.requireNonNull(entries, "DailyRanking entries must not be null");
+        validateGameAndDateMatch(game, date, entries);
+        validateEntriesPointsDescending(entries);
+    }
+
+    private static void validateEntriesPointsDescending(List<DailyEntry> entries) {
+
+        for (int i = 1; i < entries.size(); i++) {
+            if (entries.get(i - 1).points() < entries.get(i).points()) {
+                throw new IllegalArgumentException("DailyRanking entries must be sorted by points in descending order");
+            }
+        }
+    }
+
+    private static void validateGameAndDateMatch(Game game, LocalDate date, List<DailyEntry> entries) {
 
         if (entries.stream().anyMatch(entry -> !game.equals(entry.game()) || !date.equals(entry.date()))) {
             throw new IllegalArgumentException("DailyRanking entries must match ranking game and date");
         }
-
-        entries = entries.stream()
-                .sorted(BY_POINTS_DESC)
-                .toList();
     }
 
     public Week getWeek() {
--- src/main/java/com/pretz/geographic/application/domain/service/BaseDailyRankingCalculator.java
+++ src/main/java/com/pretz/geographic/application/domain/service/BaseDailyRankingCalculator.java
@@ -5,9 +5,12 @@ import com.pretz.geographic.application.domain.model.DailyRanking;
 import com.pretz.geographic.application.domain.model.Game;
 
 import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.List;
 
 public class BaseDailyRankingCalculator implements DailyRankingCalculator {
+
+    private static final Comparator<DailyEntry> BY_POINTS_DESC = Comparator.comparingInt(DailyEntry::points).reversed();
 
     @Override
     public DailyRanking calculateDailyRanking(List<DailyEntry> entries, Game game, LocalDate requestedDate) {
@@ -15,6 +18,7 @@ public class BaseDailyRankingCalculator implements DailyRankingCalculator {
         return new DailyRanking(game, requestedDate, entries.stream()
                 .filter(ent -> requestedDate.equals(ent.date()))
                 .filter(ent -> game.equals(ent.game()))
+                .sorted(BY_POINTS_DESC)
                 .toList());
     }
 }
