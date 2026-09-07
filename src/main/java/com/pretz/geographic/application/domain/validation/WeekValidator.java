20bf878a039dfcf7bf34a9ec39226fac789c3c49
Przemyslaw Etz <przemyslaw.etz@gmail.com>
2026-09-07 17:03:50 +0200

    [GEOG-10] test: Add unit tests for date interval method in DailyRankingServiceTest & minor error message refactor in WeekValidator

--- src/main/java/com/pretz/geographic/application/domain/validation/WeekValidator.java
+++ src/main/java/com/pretz/geographic/application/domain/validation/WeekValidator.java
@@ -7,14 +7,15 @@ import java.time.temporal.WeekFields;
 
 public class WeekValidator {
 
-    private static final String INVALID_YEAR_RANKING_MESSAGE = "Weekly ranking is available only for past dates while input year %s doesn't fulfill this condition";
-    private static final String INVALID_WEEK_RANKING_MESSAGE = "Weekly ranking is available only for past dates while input week %s doesn't fulfill this condition";
+    private static final String INVALID_RANKING_MESSAGE = "Weekly ranking is available only for past dates while input %s %s doesn't fulfill this condition";
+    private static final String YEAR = "year";
+    private static final String WEEK = "week";
 
     public void validate(Week week) {
 
         if (week.year() > LocalDate.now().getYear())
-            throw new InvalidDateException(String.format(INVALID_YEAR_RANKING_MESSAGE, week.year()));
+            throw new InvalidDateException(String.format(INVALID_RANKING_MESSAGE, YEAR, week.year()));
         else if (week.number() >= LocalDate.now().get(WeekFields.ISO.weekOfWeekBasedYear()))
-            throw new InvalidDateException(String.format(INVALID_WEEK_RANKING_MESSAGE, week.year()));
+            throw new InvalidDateException(String.format(INVALID_RANKING_MESSAGE, WEEK, week.year()));
     }
 }
