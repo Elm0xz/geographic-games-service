1b61e8540b0b4cd3b1f43e3581ae2a069e3d824b
Przemyslaw Etz <przemyslaw.etz@gmail.com>
2026-09-14 12:31:36 +0200

    [GEOG-16] refactor: Remove unused DailyEntryPersistenceAdapterMethod

--- src/main/java/com/pretz/geographic/application/port/out/LoadDailyEntriesPort.java
+++ src/main/java/com/pretz/geographic/application/port/out/LoadDailyEntriesPort.java
@@ -8,12 +8,6 @@ import java.util.List;
 
 public interface LoadDailyEntriesPort {
 
-    /**
-     * Loads all daily entries for the given game on the requested date.
-     * <p>
-     */
-    List<DailyEntry> loadEntries(Game game, LocalDate date);
-
     List<DailyEntry> loadEntries(List<Game> games, LocalDate date);
 
     List<DailyEntry> loadEntries(List<Game> games, LocalDate from, LocalDate to);
